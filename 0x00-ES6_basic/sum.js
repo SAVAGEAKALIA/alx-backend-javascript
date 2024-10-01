a54f1c56862690160fe210f11384da106bb48f61
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-01 03:43:16 +0000

    Check Task 0

--- 0x00-ES6_basic/sum.js
+++ 0x00-ES6_basic/sum.js
@@ -1,7 +1,7 @@
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
 
-module.exports = sum
+module.exports = sum;
 
-//console.log(sum(3, 5)); // Output: 8
+// console.log(sum(3, 5)); // Output: 8
