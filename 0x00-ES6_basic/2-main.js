50519114508c9d72f67c82b4b3e0aa20bba42ac5
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-05 12:43:21 +0100

    ES6-Basics Done

---
+++ 0x00-ES6_basic/2-main.js
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
