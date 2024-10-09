620e862b2085fcf811e537f6390676f6eae842be
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-09 08:45:59 +0100

    ES6-Promise Done 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
-    body: 'Success',
+    body: 'success',
   }))
-    .catch(() => {
-      Error();
-    })
+    .catch(() => ({}))
     .finally(() => {
       console.log('Got a response from the API');
     });
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,39 +2,49 @@
 import { createUser, uploadPhoto } from './utils';
 // const {uploadPhoto, createUser} = require('./utils')
 
+// export default function handleProfileSignup() {
+//   Promise.all([uploadPhoto(), createUser()])
+//     .then((value) => {
+//       // console.log(`${value.body} ${value.firstName} ${value.lastName}`)
+//       let str = '';
+//       const array = [];
+//       for (const val of value) {
+//         for (const dict in val) {
+//           if (val[dict] === 200) {
+//             // Do Nothing
+//           } else {
+//             // if (dict === Object.keys(val)[Object.keys(val).length-1]){
+//             //     str += val[dict]
+//             // }
+//             // else {
+//             //     str += val[dict] + " "
+//             // }
+//             array.push(val[dict]);
+//           }
+//         }
+//       }
+//       // console.log(array);
+//       for (let i = 0; i < array.length; i++) {
+//         // str += array[i]
+//         if (i === array.length - 1) {
+//           str += array[i];
+//         } else {
+//           str += `${array[i]} `;
+//         }
+//       }
+//       console.log(str);
+//     })
+//     .catch((err) => {
+//       console.log('Signup system offline');
+//     });
+// }
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      // console.log(`${value.body} ${value.firstName} ${value.lastName}`)
-      let str = '';
-      const array = [];
-      for (const val of value) {
-        for (const dict in val) {
-          if (val[dict] === 200) {
-            // Do Nothing
-          } else {
-            // if (dict === Object.keys(val)[Object.keys(val).length-1]){
-            //     str += val[dict]
-            // }
-            // else {
-            //     str += val[dict] + " "
-            // }
-            array.push(val[dict]);
-          }
-        }
-      }
-      // console.log(array);
-      for (let i = 0; i < array.length; i++) {
-        // str += array[i]
-        if (i === array.length - 1) {
-          str += array[i];
-        } else {
-          str += `${array[i]} `;
-        }
-      }
-      console.log(str);
-    })
-    .catch((err) => {
-      console.log('Signup system offline');
-    });
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((values) => {
+            const [photo, user] = values;
+            console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+        })
+        .catch(() => {
+            console.log('Signup system offline');
+        });
 }
