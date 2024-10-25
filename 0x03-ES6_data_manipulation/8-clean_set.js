2ee6d2fb55604a42e1587c6e71ac28072cb78230
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-10-25 05:09:26 +0100

    ES6-Data Manipulation Done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
     if (startString === '') {
         return '';
     }
-    const newString = [...set].filter((str) => str.substring(0, 3) === startString).map((str) => str.slice(3));
+    const newString = [...set].filter((str) => str.startsWith(startString)).map((str) => str.slice(3));
     // console.log(`${newString} newString`);
     return newString.join('-');
 }
